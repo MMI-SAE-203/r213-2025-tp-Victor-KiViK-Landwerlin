5a544e80e7ed284e604a3ed74fcb764597d9f7e7
Victor-KiViK-Landwerlin <150051582+Victor-KiViK-Landwerlin@users.noreply.github.com>
2025-01-17 15:23:04 +0100

    nouvelle offre

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,18 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			keyframes: {
+				"rainbow":{
+					"0%": {color: "red"},
+					"20%": {color: "orange"},
+					"40%": {color: "yellow"},
+					"60%": {color: "green"},
+					"80%": {color: "blue"},
+					"100%": {color: "purple"},
+				}
+		},
 	},
 	plugins: [],
+}
 }
